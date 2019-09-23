ac2812a636b8f6b86bd328f4ce3118d49ef151b1
Kaique da Silva <kaeyosthaeron@gmail.com>
2019-09-23 10:23:09 -0300

    Changed to requests because of redirects

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-const url = require('url');
-const http = require('http');
-const https = require('https');
+const requests = require('requests');
 const args = process.argv.slice(2);
-const rawOptions = url.parse(args[0]);
-const options = {
-    hostname: rawOptions.host,
-    port: rawOptions.port || (rawOptions.host.indexOf('https') > -1 ? 443 : 80),
-    path: rawOptions.path,
-};
-const request = rawOptions.host.indexOf('https') > -1 ? https.request : http.request;
-const req = request(options, res => {
-    if (args.length === 1) {
-        res.pipe(process.stdout);
-    } else if (args.length === 2 && args[1] === '--status-code') {
-        console.log(`Status code: ${res.statusCode}`);
-    }
-}); req.end();
+
+let req = requests(args[0], { streaming: true });
+
+req.on('data', console.log.bind(console))
